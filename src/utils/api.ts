0388a4c41423777cd6eb85ea07d85b71eb15ed1f
André Melo <andrelucas@zhavia.com.br>
2024-06-19 15:32:14 -0300

    feat: add chamadas de API para o DB postgres

---
+++ src/utils/api.ts
@@ -0,0 +1,64 @@
+'use server';
+
+import { sql } from '@vercel/postgres';
+
+const validateUser = (username: string) => {
+  if (!username || username.length !== 5) {
+    return {
+      error: 'Usuario inválido, o mesmo deve ter 5 letras.',
+    };
+  }
+  return null;
+};
+
+export async function createUser(data: { username: string; }) {
+  const { username } = data;
+
+  const validationError = validateUser(username);
+  if (validationError) {
+    return { status: 400, ...validationError };
+  }
+
+  try {
+    const existingUserResult =
+      await sql`SELECT * FROM users WHERE username = ${username}`;
+    if (existingUserResult.rows.length > 0) {
+      return { status: 201, error: 'Usuário já existe.' };
+    }
+
+    await sql`INSERT INTO users (username) VALUES (${username})`;
+    return { status: 200, message: 'Usuário criado com sucesso.' };
+  } catch (error) {
+    return { status: 400, error: 'An error occurred while creating the user.' };
+  }
+}
+
+export async function sendScore(user: string, difficulty: string) {
+  try {
+    const userResult = await sql`SELECT * FROM users WHERE username = ${user}`;
+
+    if (userResult.rows.length === 0) {
+      return { status: 404, error: 'Usuário não encontrado.' };
+    }
+
+    const scoreResult = await sql`SELECT * FROM ranking WHERE username = ${user} and difficulty = ${difficulty}`;
+    if (scoreResult.rows.length === 0) {
+      await sql`INSERT INTO ranking (username, score, difficulty) VALUES (${user}, 1, ${difficulty})`;
+    } else {
+      await sql`UPDATE ranking SET score = score + 1 WHERE username = ${user} and difficulty = ${difficulty}`;
+    }
+
+    return { status: 200, message: 'Pontuação enviada com sucesso.' };
+  } catch (error) {
+    return { status: 400, error: 'An error occurred while sending the score.' };
+  }
+}
+
+export async function getRanking() {
+  try {
+    const result = await sql`SELECT username, SUM(score) as qtd FROM ranking GROUP BY username ORDER BY qtd DESC LIMIT 10`;
+    return { status: 200, data: result.rows };
+  } catch (error) {
+    return { status: 500, error: 'An error occurred while fetching the ranking.' };
+  }
+}
