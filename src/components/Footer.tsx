7aa3a5fd3507d1952edd1109040319709bd38108
André Melo <andrelucas@zhavia.com.br>
2024-06-20 17:02:59 -0300

    fix: fix footer bye fontawesome

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <footer className="py-4 text-center flex flex-col justify-center">
       <p>&copy; {new Date().getFullYear()} - André Melo</p>
-      <p>
+      <p className="flex items-center justify-center">
         Feito com
         <a
           href="https://www.youtube.com/watch?v=kpnW68Q8ltc&pp=ygUZZG9vbSBhbGwgdGhleSBmZWFyIGlzIHlvdQ%3D%3D"
@@ -22,15 +22,44 @@ const Footer = () => {
           >
             {hover ? '😈' : '❤️'}
           </span>
-          .
         </a>
         <a
           href="https://github.com/andrelsmelo"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-blue-500 ml-2"
+          className="w-4 h-4 hover:cursor-pointer mx-2 hover:scale-110"
         >
-          <i className="fab fa-github"></i>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 256 256"
+            className="w-4 h-4"
+          >
+            <rect width="256" height="256" fill="none" />
+            <path
+              d="M119.83,56A52,52,0,0,0,76,32a51.92,51.92,0,0,0-3.49,44.7A49.28,49.28,0,0,0,64,104v8a48,48,0,0,0,48,48h48a48,48,0,0,0,48-48v-8a49.28,49.28,0,0,0-8.51-27.3A51.92,51.92,0,0,0,196,32a52,52,0,0,0-43.83,24Z"
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="16"
+            />
+            <path
+              d="M104,232V192a32,32,0,0,1,32-32h0a32,32,0,0,1,32,32v40"
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="16"
+            />
+            <path
+              d="M104,208H72a32,32,0,0,1-32-32A32,32,0,0,0,8,144"
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="16"
+            />
+          </svg>
         </a>
       </p>
     </footer>
