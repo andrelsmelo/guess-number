775931fe4e86dd40051c976dbb042e2d5d01187c
André Melo <andrelucas@zhavia.com.br>
2024-06-24 13:57:14 -0300

    feat: add border && fix: text size

--- src/components/DifficultySelection.tsx
+++ src/components/DifficultySelection.tsx
@@ -9,13 +9,13 @@ const DifficultySelection: FC<Props> = ({ startGame }) => {
   return (
     <div className="flex flex-col justify-center items-center gap-8 md:gap-16">
       <h1 className="text-5xl text-center">Acerte o número entre 1 a 100</h1>
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center border p-4 rounded-md">
         <h4>Pontuações de Ranking</h4>
-        <span className="text-xs">Fácil: 5 pontos</span>
-        <span className="text-xs">Médio: 10 pontos</span>
-        <span className="text-xs">Difícil: 100 pontos</span>
-        <span className="text-xs">Pesadelo: 500 pontos</span>
-        <span className="text-xs">Inferno: 1000 pontos</span>
+        <span className="text-base">Fácil: 5 pontos</span>
+        <span className="text-base">Médio: 10 pontos</span>
+        <span className="text-base">Difícil: 100 pontos</span>
+        <span className="text-base">Pesadelo: 500 pontos</span>
+        <span className="text-base">Inferno: 1000 pontos</span>
       </div>
       <h2 className="text-2xl mb-4">Escolha a dificuldade do jogo</h2>
       <div className="flex space-x-4 justify-around">
